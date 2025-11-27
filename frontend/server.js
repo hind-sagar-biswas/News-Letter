f24064998aa57d099a564e97bf23b0e49e35246d
Shamim Islam <shamimislam12231@gmail.com>
2025-11-28 02:49:43 +0600

    minor fixes

--- frontend/server.js
+++ frontend/server.js
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 const { createServer } = require("http");
 const next = require("next");
-const { parse } = require("url");
+const { URL } = require("url");
 
 const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== "production";
@@ -10,7 +10,19 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const parsedUrl = {
+      pathname: url.pathname,
+      query: Object.fromEntries(url.searchParams),
+      search: url.search,
+      hash: url.hash,
+      href: url.href,
+      origin: url.origin,
+      host: url.host,
+      hostname: url.hostname,
+      port: url.port,
+      protocol: url.protocol,
+    };
     handle(req, res, parsedUrl);
   }).listen(port, (err) => {
     if (err) throw err;
